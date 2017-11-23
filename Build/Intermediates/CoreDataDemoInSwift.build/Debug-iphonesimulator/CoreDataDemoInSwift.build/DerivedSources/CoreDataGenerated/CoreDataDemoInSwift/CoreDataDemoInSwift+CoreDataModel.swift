6bd622e2864378e84ccd15741e73f0e6487cbf29
vijay <vdmasal@gmail.com>
2017-11-23 15:19:18 +0530

    Basic Core Data demo in Swift

---
+++ Build/Intermediates/CoreDataDemoInSwift.build/Debug-iphonesimulator/CoreDataDemoInSwift.build/DerivedSources/CoreDataGenerated/CoreDataDemoInSwift/CoreDataDemoInSwift+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataDemoInSwift+CoreDataModel.swift
+//  
+//
+//  Created by Emberify_Vijay on 23/11/17.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
